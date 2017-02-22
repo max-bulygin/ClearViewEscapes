4f3d6ff3d9089318e5a2237cc8b6ed077a24f70b
Max Bulygin <maksym.bulygin@gmail.com>
2017-02-22 18:31:49 +0300

    postcss workflow edited

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
+'use strict'
+
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const customProps = require('postcss-custom-properties');
+const cssImport = require('postcss-import');
 
 gulp.task('html', () => console.log("html"));
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([customProps, autoprefixer]))
+        .pipe(postcss([cssImport, customProps, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
