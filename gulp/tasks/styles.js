70d8940747ccf9f7ba13338d7a5b88fafc2231df
Max Bulygin <maksym.bulygin@gmail.com>
2017-02-22 22:25:51 +0300

    responsive text setup

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,12 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const customProps = require('postcss-custom-properties');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
+const nesting = require('postcss-nesting');
 
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, customProps, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, nesting,customProps, autoprefixer]))
         .on('error', (error) => {
             gutil.log(error.message);
             this.emit('end');
